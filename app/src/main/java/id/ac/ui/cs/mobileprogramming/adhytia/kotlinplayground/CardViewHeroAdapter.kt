c66313d4f8137c8c77b1beb8ad6e3141599fbc34
Adhytia <adit.14.12.99@gmail.com>
2021-03-15 21:31:52 +0700

    Cardview using viewbinding

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/CardViewHeroAdapter.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/CardViewHeroAdapter.kt
@@ -1,64 +1,64 @@
 package id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground.databinding.ItemCardviewHeroBinding
 
 class CardViewHeroAdapter(private val listHero: ArrayList<Hero>) :
     RecyclerView.Adapter<CardViewHeroAdapter.CardViewViewHolder>() {
-    class CardViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
-        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
-        var tvDetail: TextView = itemView.findViewById(R.id.tv_item_detail)
-        var btnFavorite: Button = itemView.findViewById(R.id.btn_set_favorite)
-        var btnShare: Button = itemView.findViewById(R.id.btn_set_share)
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewViewHolder {
-        val view: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_cardview_hero, parent, false)
-        return CardViewViewHolder(view)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): CardViewViewHolder {
+        val binding = ItemCardviewHeroBinding.inflate(
+            LayoutInflater.from(viewGroup.context),
+            viewGroup,
+            false
+        )
+        return CardViewViewHolder(binding)
+
     }
 
     override fun onBindViewHolder(holder: CardViewViewHolder, position: Int) {
-        val hero = listHero[position]
-        Glide.with(holder.itemView.context)
-            .load(hero.photo)
-            .apply(RequestOptions().override(350, 550))
-            .into(holder.imgPhoto)
-        holder.tvName.text = hero.name
-        holder.tvDetail.text = hero.detail
-        holder.btnFavorite.setOnClickListener {
-            Toast.makeText(
-                holder.itemView.context,
-                "Favorite " + listHero[holder.adapterPosition].name,
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-        holder.btnShare.setOnClickListener {
-            Toast.makeText(
-                holder.itemView.context,
-                "Share " + listHero[holder.adapterPosition].name,
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-        holder.itemView.setOnClickListener {
-            Toast.makeText(
-                holder.itemView.context,
-                "Kamu memilih " + listHero[holder.adapterPosition].name,
-                Toast.LENGTH_SHORT
-            ).show()
-        }
+        holder.bind(listHero[position])
     }
 
-    override fun getItemCount(): Int {
-        return listHero.size
+    override fun getItemCount(): Int = listHero.size
+
+    inner class CardViewViewHolder(private val binding: ItemCardviewHeroBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(hero: Hero) {
+            with(binding) {
+                Glide.with(itemView.context)
+                    .load(hero.photo)
+                    .apply(RequestOptions().override(350, 550))
+                    .into(imgItemPhoto)
+                tvItemName.text = hero.name
+                tvItemDescription.text = hero.description
+                btnSetFavorite.setOnClickListener {
+                    Toast.makeText(
+                        itemView.context,
+                        "Favorite ${hero.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                btnSetShare.setOnClickListener {
+                    Toast.makeText(
+                        itemView.context,
+                        "Share ${hero.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                itemView.setOnClickListener {
+                    Toast.makeText(
+                        itemView.context,
+                        "Kamu memilih ${hero.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/MainActivity.kt
@@ -3,19 +3,22 @@ package id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var rvHeroes: RecyclerView
+    private var title = "Mode List"
     private val list = ArrayList<Hero>()
-
-
+    private var mode: Int = 0
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val STATE_TITLE = "state_string"
+        private const val STATE_LIST = "state_list"
+        private const val STATE_MODE = "state_mode"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +27,28 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvHeroes.setHasFixedSize(true)
 
-        list.addAll(getListHeroes())
-        showRecyclerList()
+        if (savedInstanceState == null) {
+            setActionBarTitle(title)
+            list.addAll(getListHeroes())
+            showRecyclerList()
+            mode = R.id.action_list
+        } else {
+            title = savedInstanceState.getString(STATE_TITLE).toString()
+            val stateList = savedInstanceState.getParcelableArrayList<Hero>(STATE_LIST)
+            val stateMode = savedInstanceState.getInt(STATE_MODE)
+            setActionBarTitle(title)
+            if (stateList != null) {
+                list.addAll(stateList)
+            }
+            setMode(stateMode)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_TITLE, title)
+        outState.putParcelableArrayList(STATE_LIST, list)
+        outState.putInt(STATE_MODE, mode)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -86,5 +109,11 @@ class MainActivity : AppCompatActivity() {
         binding.rvHeroes.layoutManager = GridLayoutManager(this, 2)
         val gridHeroAdapter = GridHeroAdapter(list)
         binding.rvHeroes.adapter = gridHeroAdapter
+    }
+
+    private fun showRecyclerCardView() {
+        binding.rvHeroes.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewHeroAdapter(list)
+        binding.rvHeroes.adapter = cardViewHeroAdapter
     }
 }
