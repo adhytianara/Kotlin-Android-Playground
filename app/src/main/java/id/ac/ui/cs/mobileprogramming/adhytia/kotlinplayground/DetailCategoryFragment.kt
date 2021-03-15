c4fa55463927f23c874834334f71fba717dce7e7
Adhytia <adit.14.12.99@gmail.com>
2021-03-15 22:17:45 +0700

    Send data using bundle

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/DetailCategoryFragment.kt
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground.databinding.FragmentDetailCategoryBinding
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
