6e60570989928d6c7c04854021e4a3698979b00a
Adhytia <adit.14.12.99@gmail.com>
2021-03-16 16:45:00 +0700

    Basic optionmenu

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/MenuActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
