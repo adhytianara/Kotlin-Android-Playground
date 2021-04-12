611a2501296abaa4555576af64a474c397c2df11
Adhytia <adit.14.12.99@gmail.com>
2021-04-12 23:48:26 +0700

    implement Content Provider (Consumer App)

---
+++ consumerapp/src/main/java/bangkit/adhytia/consumerapp/db/DatabaseContract.kt
@@ -0,0 +1,28 @@
+package bangkit.adhytia.consumerapp.db
+
+import android.net.Uri
+import android.provider.BaseColumns
+
+object DatabaseContract {
+
+    const val AUTHORITY = "id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground"
+    const val SCHEME = "content"
+
+    class NoteColumns : BaseColumns {
+
+        companion object {
+            const val TABLE_NAME = "note"
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val DATE = "date"
+
+            // untuk membuat URI content://id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground/note
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
+        }
+
+    }
+}
