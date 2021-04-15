7f6b161148c2bd82bab800467dee91a3ea73844a
Adhytia <adit.14.12.99@gmail.com>
2021-04-15 15:27:48 +0700

    feat : Widgets with Button

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/UpdateWidgetService.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/UpdateWidgetService.kt
@@ -10,8 +10,8 @@ class UpdateWidgetService : JobService() {
 
     override fun onStartJob(jobParameters: JobParameters): Boolean {
         val manager = AppWidgetManager.getInstance(this)
-        val view = RemoteViews(packageName, R.layout.random_number_widget)
-        val theWidget = ComponentName(this, RandomNumberWidget::class.java)
+        val view = RemoteViews(packageName, R.layout.random_numbers_widget)
+        val theWidget = ComponentName(this, RandomNumbersWidget::class.java)
         val lastUpdate = "Random: " + NumberGenerator.generate(100)
         view.setTextViewText(R.id.appwidget_text, lastUpdate)
         manager.updateAppWidget(theWidget, view)
