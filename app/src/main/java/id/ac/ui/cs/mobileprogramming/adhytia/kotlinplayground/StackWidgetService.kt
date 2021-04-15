a782177700cbf480279d1effa3e541db930d6bfc
Adhytia <adit.14.12.99@gmail.com>
2021-04-15 16:28:29 +0700

    feat: implement StackView Widget

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/kotlinplayground/StackWidgetService.kt
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.mobileprogramming.adhytia.kotlinplayground
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+}
